4ba403cc19adaa70873dcc23cc67c3e6b47b049f
Thomas Schmidt <thomas.schmidt@gastfreund.net>
2018-09-13 09:18:50 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # datev_extract
 This npm package is a simple Extractor for Salary PDFs from the DateV platform arbeitnehmeronline.de
+
+## Setup
+
+1. Clone the Github Repo in a Directory of your choice via
+
+> git clone https://github.com/ThomasSchmidtGF/datev_extract.git
+
+2. Navigate in your chosen Directory, open a CLI and install the package via
+
+> npm i
+
+3. Put your Login Credentials into the cred.js file (no worries, nobody except for you, puppeteer and datev is going to see them)
+
+## Usage
+
+Just open a CLI and enter
+
+> npm lohn.js
+
+Done! Your PDF is in your Downloads folder.
+
+If you want to see what the script is doing, open lohn.js with a editor and set the option "headless" to false.
---
+++ lohn.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env nodejs
+const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
+function getSmsCode(){
+    var sms = readlineSync.question('Enter your Smscode: ');
+    return sms;
+  
+}
+
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
+
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    ignoreHTTPSErrors: true
+  })
+  const page = await browser.newPage()
+  const CREDS = require('./cred');
+  await page.setViewport({ width: 1920, height: 1024 });
+  await page.goto('http://www.datev.de/ano/');
+  await page.click('#loginContainer > div > ul > li:nth-child(1) > a > div > div.media-body');
+  await page.waitForSelector('#username')
+  await page.click('#username');
+  await page.keyboard.type(CREDS.username);
+  await page.click('#password');
+  await page.keyboard.type(CREDS.password);
+  await page.click('body > main > form > button');
+  await page.waitForNavigation();
+  await page.click(' #mTAN');
+  await page.keyboard.type(getSmsCode());
+  await page.click('body > main > form > button');
+  await delay(5000);
+  await page.waitForSelector('#navSTARTHEADLINEDOCS > a');
+  await page.click('#navSTARTHEADLINEDOCS > a');
+  await page.waitForSelector('#dijit__TemplatedMixin_19 > table > tbody > tr > td:nth-child(2) > div.analysisCaption');
+  await page.click('#dijit__TemplatedMixin_19 > table > tbody > tr > td:nth-child(1) > input[type="checkbox"]');
+  await page.click("#btnDownloadZip");
+  await delay(2000);
+  await page.click('#top > div.infobar > ul > li:nth-child(2) > a');
+  await browser.close();
+}
+
+
+run();
